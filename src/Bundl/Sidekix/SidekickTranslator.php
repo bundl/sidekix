1ad0cc7583f4e7845cc35f96f409543a108435e5
Brooke Bryan <brooke@justdevelop.it>
2013-11-22 09:43:14 +0000

    Update translator to implement ITranslator

---
+++ src/Bundl/Sidekix/SidekickTranslator.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author  brooke.bryan
+ */
+
+namespace Bundl\Sidekix;
+
+use Cubex\Foundation\Config\ConfigTrait;
+use Cubex\Foundation\Container;
+use Cubex\I18n\Translator\ITranslator;
+
+class SidekickTranslator implements ITranslator
+{
+  use ConfigTrait;
+
+  public function translate($text, $sourceLanguage, $targetLanguage)
+  {
+    $config       = Container::config()->get("sidekix", new Config());
+    $translateApi = $config->getStr("translate_endpoint", null);
+    $projectId    = $config->getStr("project_id", 0);
+    if($translateApi !== null)
+    {
+      $postData = [
+        'text'      => $text,
+        'source'    => $sourceLanguage,
+        'target'    => $targetLanguage,
+        'projectId' => $projectId,
+      ];
+
+      return \Requests::post($translateApi, [], $postData)->body;
+    }
+    else
+    {
+      throw new \Exception(
+        "No Translate Endpoint specified in config file", 400
+      );
+    }
+  }
+}
--- src/Bundl/Sidekix/SidekixBundl.php
+++ src/Bundl/Sidekix/SidekixBundl.php
@@ -48,28 +48,4 @@ class SidekixBundl extends Bundle
       }
     }
   }
-
-  public function translate($text, $sourceLanguage, $targetLanguage)
-  {
-    $config       = Container::config()->get("sidekix", new Config());
-    $translateApi = $config->getStr("translate_endpoint", null);
-    $projectId    = $config->getStr("project_id", 0);
-    if($translateApi !== null)
-    {
-      $postData = [
-        'text'      => $text,
-        'source'    => $sourceLanguage,
-        'target'    => $targetLanguage,
-        'projectId' => $projectId,
-      ];
-
-      return \Requests::post($translateApi, [], $postData)->body;
-    }
-    else
-    {
-      throw new \Exception(
-        "No Translate Endpoint specified in config file", 400
-      );
-    }
-  }
 }
